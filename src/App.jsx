d1b83115b8035ef85a6831c3a4cdad25dde46eb9
Lasma Poksane <laasmapoksane@gmail.com>
2023-08-19 01:38:44 +0200

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="app">
+      <h1 className="app-heading">Welcome to the Goalkeeper.</h1>
+      <p className="app-info">
+        This is an app where you goals can be added for an upcoming week, a
+        month or a year to have a organized list of all the activities want to
+        be done. Once the respective day, month or year ends, the list is empty
+        for you to add new goals!
       </p>
-    </>
-  )
+      {/* A form to add goals will be here */}
+      {/* Goals will be displayed here */}
+    </div>
+  );
 }
 
-export default App
+export default App;
