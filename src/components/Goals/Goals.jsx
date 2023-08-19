a0b21e1d6f90e0d4b6a5c36867e3b317382f6121
Lasma Poksane <laasmapoksane@gmail.com>
2023-08-19 13:40:15 +0200

    create initial Goals component

---
+++ src/components/Goals/Goals.jsx
@@ -0,0 +1,11 @@
+const Goals = () => {
+  return (
+    <div>
+      {/* Daily goals will be here */}
+      {/* Weekly goals will be here */}
+      {/* Yearly goals will be here */}
+    </div>
+  );
+};
+
+export default Goals;
