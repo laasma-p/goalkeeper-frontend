f348acb8e0028400a5baf895f541ec2707da5875
Lasma Poksane <laasmapoksane@gmail.com>
2023-08-20 00:41:11 +0200

    add className prop to Card component

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,5 +1,7 @@
-const Card = ({ children }) => {
-  return <div>{children}</div>;
+const Card = ({ children, className }) => {
+  const classes = `${className}`;
+
+  return <div className={classes}>{children}</div>;
 };
 
 export default Card;
