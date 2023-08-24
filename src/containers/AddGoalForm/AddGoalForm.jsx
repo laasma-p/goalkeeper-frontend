cecd6191c506a880689e0b394a368e7546c1b554
Lasma Poksane <laasmapoksane@gmail.com>
2023-08-24 20:58:02 +0200

    make the inputs controllable

--- src/containers/AddGoalForm/AddGoalForm.jsx
+++ src/containers/AddGoalForm/AddGoalForm.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
 import classes from "./AddGoalForm.module.css";
 
 const AddGoalForm = () => {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [chosenAddToGoals, setChosenAddToGoals] = useState("daily");
+
+  const enteredGoalChangeHandler = (event) => {
+    setEnteredGoal(event.target.value);
+  };
+
+  const chosenAddToGoalsChangeHandler = (event) => {
+    setChosenAddToGoals(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredGoal("");
+    setChosenAddToGoals("daily");
+  };
+
   return (
     <div className={classes["add-goal"]}>
-      <form className={classes["add-goal-form"]}>
+      <form className={classes["add-goal-form"]} onSubmit={submitHandler}>
         <div className={classes["form-control"]}>
           <label htmlFor="goal">Goal</label>
-          <input type="text" id="goal" name="goal-name" />
+          <input
+            type="text"
+            id="goal"
+            name="goal-name"
+            onChange={enteredGoalChangeHandler}
+            value={enteredGoal}
+          />
         </div>
 
         <div className={classes["form-control"]}>
           <label htmlFor="goals">Select where to add it</label>
-          <select name="goals" id="goals">
+          <select
+            name="goals"
+            id="goals"
+            onChange={chosenAddToGoalsChangeHandler}
+            value={chosenAddToGoals}
+          >
             <option value="daily">Day goal</option>
-            <option value="monthly">Week goal</option>
+            <option value="weekly">Week goal</option>
             <option value="yearly">Year goal</option>
           </select>
         </div>
