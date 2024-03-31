e9fbcf70044b0abf267c2ab78e9dcb9d261c8200
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-31 20:03:20 +0200

    add /register and /login routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Register from "./containers/Register/Register.jsx";
+import Login from "./containers/Login/Login.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
