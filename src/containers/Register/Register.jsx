97cd7610da62520f8b8465ede4154ac3fa214204
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-31 22:00:37 +0200

    create initial Register component

---
+++ src/containers/Register/Register.jsx
@@ -0,0 +1,54 @@
+import classes from "./Register.module.css";
+import { useState } from "react";
+
+const Register = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const enteredEmailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const enteredPasswordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={classes.register}>
+      <h1 className={classes["register-heading"]}>Register</h1>
+      <div className={classes["form-container"]}>
+        <form className={classes["register-form"]} onSubmit={handleSubmit}>
+          <label htmlFor="email" className={classes["form-label"]}>
+            Email
+          </label>
+          <input
+            type="email"
+            className={classes.input}
+            onChange={enteredEmailChangeHandler}
+            value={enteredEmail}
+            required
+          />
+          <label htmlFor="password" className={classes["form-label"]}>
+            Password
+          </label>
+          <input
+            type="password"
+            className={classes.input}
+            onChange={enteredPasswordChangeHandler}
+            value={enteredPassword}
+            required
+          />
+          <button type="submit" className={classes["register-button"]}>
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
