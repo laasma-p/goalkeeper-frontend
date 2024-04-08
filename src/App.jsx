1d7cdb04a8697f420b7bd1bd6608f5dfebed8ab9
Lasma Poksane <laasmapoksane@gmail.com>
2024-04-08 13:26:02 +0200

    render Register, Login and Goals components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import Goals from "./components/Goals/Goals";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="goals" element={<Goals />} />
       </Routes>
     </>
   );
