c77b8c4381906335d79850a768ddf37c3059baa4
Lasma Poksane <laasmapoksane@gmail.com>
2024-04-12 00:10:09 +0200

    fetch goals from database

--- src/components/Goals/Goals.jsx
+++ src/components/Goals/Goals.jsx
@@ -4,16 +4,46 @@ import YearlyGoals from "../YearlyGoals/YearlyGoals";
 import AddGoalForm from "../../containers/AddGoalForm/AddGoalForm";
 import Header from "../Header/Header";
 import classes from "./Goals.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Goals = () => {
   const [dailyGoals, setDailyGoals] = useState([]);
   const [weeklyGoals, setWeeklyGoals] = useState([]);
   const [yearlyGoals, setYearlyGoals] = useState([]);
+
+  const fetchGoals = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/goals");
+      const goalsData = response.data;
+
+      const dailyGoalsData = goalsData.filter(
+        (goal) => goal.category === "daily"
+      );
+      const weeklyGoalsData = goalsData.filter(
+        (goal) => goal.category === "weekly"
+      );
+      const yearlyGoalsData = goalsData.filter(
+        (goal) => goal.category === "yearly"
+      );
+
+      setDailyGoals(dailyGoalsData);
+      setWeeklyGoals(weeklyGoalsData);
+      setYearlyGoals(yearlyGoalsData);
+    } catch (error) {
+      console.log("Cannot fetch goals", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+
   return (
     <>
       <Header />
       <div className={classes.goals}>
-        <AddGoalForm />
+        <AddGoalForm fetchUpdatedGoals={fetchGoals} />
         <DailyGoals dailyGoals={dailyGoals} />
         <WeeklyGoals weeklyGoals={weeklyGoals} />
         <YearlyGoals yearlyGoals={yearlyGoals} />
