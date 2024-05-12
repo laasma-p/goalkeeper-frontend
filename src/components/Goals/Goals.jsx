c9b4e4e0c8f68cbfe219fb473b2049799d52472a
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-12 03:47:57 +0200

    check for token's existence in localStorage

--- src/components/Goals/Goals.jsx
+++ src/components/Goals/Goals.jsx
@@ -6,11 +6,22 @@ import Header from "../Header/Header";
 import classes from "./Goals.module.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Goals = () => {
   const [dailyGoals, setDailyGoals] = useState([]);
   const [weeklyGoals, setWeeklyGoals] = useState([]);
   const [yearlyGoals, setYearlyGoals] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    } else {
+      fetchGoals();
+    }
+  }, [navigate]);
 
   const fetchGoals = async () => {
     try {
